fd0ac621ad612eb874c5a68b42fbc8fcd734867e
GXY <903367226@qq.com>
2017-09-23 21:31:07 +0800

    导入初始版本

---
+++ User/Picture.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+//胜利手势
+const uint8_t Pic0[72] = 
+{
+    0x00
+};
+
+//棒棒手势
+const uint8_t Pic1[72] = 
+{
+    0x00
+};
+
+//OK手势
+const uint8_t Pic2[72] = 
+{
+    0x00
+};
---
+++ User/Picture.h
@@ -0,0 +1,13 @@
+#ifndef _PICTURE_H_
+#define _PICTURE_H_
+
+//胜利手势
+extern const uint8_t Pic0[];
+
+//棒棒手势
+extern const uint8_t Pic1[];
+
+//OK手势
+extern const uint8_t Pic2[];
+
+#endif
